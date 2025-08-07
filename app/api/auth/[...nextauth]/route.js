8a0f9a4f7102e0fcf27f5f3c4a8806d6da42b52a
Tulasi Shukla <tulsishuklag@gmail.com>
2025-08-07 11:35:43 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,38 +1,36 @@
+
+import NextAuth from "next-auth"
 import connectDb from '@/db/connectDb'
 import User from '@/models/User'
-import NextAuth from 'next-auth'
-import GithubProvider from "next-auth/providers/github"
+import GitHubProvider from "next-auth/providers/github"
 
-export const authoptions = NextAuth({
+const handler = NextAuth({
   providers: [
-    // OAuth authentication providers...
-    GithubProvider({
+    GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     })
   ],
-      callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      if (account.provider =="github") {
-        await connectDb()
-        const currentuser = await User.findOne({email:email})
-        if (!currentuser) {
-          const newuser = await User.create({
-             email: user.email, 
-              username: user.email.split("@")[0], 
-          })
-console.log(newuser)
+  callbacks: {
+    async signIn({ user, account, profile, email }) {
+      if (account.provider === "github") {
+        await connectDb();
+        const currentUser = await User.findOne({ email: user.email });
+        if (!currentUser) {
+          await User.create({
+            email: user.email,
+            username: user.email.split("@")[0],
+          });
         }
-        return true
+        return true;
       }
     },
-    async session({ session, user, token }) {
-        const dbUser = await User.findOne({email: session.user.email})
-        session.user.name = dbUser.username
-        console.log("db",dbUser )
-        return session
-      },
+    async session({ session }) {
+      const dbUser = await User.findOne({ email: session.user.email });
+      session.user.name = dbUser.username;
+      return session;
+    },
   }
-})
+});
 
-export { authoptions as GET, authoptions as POST }
+export { handler as GET, handler as POST };
